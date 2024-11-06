657d6dff999ddc1678a43f250ec513faed8c3cd1
Krishna Chamarthy <chamarthysr@gmail.com>
2024-11-07 00:31:26 +0530

    Update firebase.js

--- src/Lib/firebase.js
+++ src/Lib/firebase.js
@@ -19,6 +19,7 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-export const auth = getAuth()
-export const db = getFirestore()
-export const storage = getStorage();
+// Pass `app` as an argument to ensure correct initialization
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
